f12692319985086a27644567a81ecca058305754
adil.gahramanli <adil.gahramanli@le-campus-numerique.fr>
2024-08-30 11:30:33 +0200

    initial protected against concurrent access with script bash for parrallel requests

---
+++ src/main/java/org/example/bddconcurrentaccess/BddConcurrentAccessApplication.java
@@ -0,0 +1,13 @@
+package org.example.bddconcurrentaccess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BddConcurrentAccessApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BddConcurrentAccessApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/bddconcurrentaccess/BddConcurrentAccessApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.bddconcurrentaccess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BddConcurrentAccessApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
