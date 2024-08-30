3f74b644938995d1cf3b476f77f371598d6c22fa
adil.gahramanli <adil.gahramanli@le-campus-numerique.fr>
2024-08-30 13:34:36 +0200

    modif bash script for parallel requests

--- src/main/java/org/example/bddconcurrentaccess/parallel_curl.sh
+++ src/main/java/org/example/bddconcurrentaccess/parallel_curl.sh
@@ -16,8 +16,7 @@ log_with_timestamp() {
 export -f log_with_timestamp
 
 # Run the curl commands in parallel and log output
-seq 1 10 | xargs -n1 -P10 -I {} bash -c 'log_with_timestamp "Request #{}: $(curl -s -o /dev/null -w "%{http_code}" -X POST -H "Content-Type: application/json" -d "5" http://localhost:8080/counters/66d18233d47f877b4225825c)"'
-
+seq 1 10 | xargs -n1 -P10 bash -c 'log_with_timestamp "Request #{}: $(curl -s -o /dev/null -w "%{http_code}" -X POST -H "Content-Type: application/json" -d "5" http://localhost:8080/counters/66d18233d47f877b4225825c)"'
 done
 
 wait
